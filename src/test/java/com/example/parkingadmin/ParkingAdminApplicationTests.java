28ae49d21f9b766c8d1129265c9be612e50fdf45
YE <djen12@naver.com>
2021-07-22 15:02:59 +0900

    test1

---
+++ src/test/java/com/example/parkingadmin/ParkingAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
